83eb45ed1ad6e20ce130e0988d304897baedb172
gunwoo99 <62701141+gunwoo99@users.noreply.github.com>
2023-12-04 20:09:18 +0900

    backend 작동 확인

--- src/main/java/comp322/NCVS/connection/ConnectionConst.java
+++ src/main/java/comp322/NCVS/connection/ConnectionConst.java
@@ -1,7 +1,7 @@
 package comp322.NCVS.connection;
 
 public abstract class ConnectionConst {
-    public static final String URL = "jdbc:oracle:thin:@192.168.56.1:1521:orcl";
-    public static final String USERNAME = "team6";
-    public static final String PASSWORD = "team6";
+    public static final String URL = "jdbc:oracle:thin:@172.30.1.25:1521:orcl";
+    public static final String USERNAME = "teamproject";
+    public static final String PASSWORD = "teamproject";
 }
